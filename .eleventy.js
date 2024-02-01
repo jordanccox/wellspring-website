164080a27c42a7e6b99b1ce2835e67cb0cc23d60
Jordan Cox <jordancox747@outlook.com>
2024-02-01 07:25:10 -0700

    portfolio images resized

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ const postCss = require("postcss");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const Image = require("@11ty/eleventy-img");
+const fs = require("fs");
 
 const postcssFilter = (cssCode, done) => {
   postCss([
@@ -19,12 +21,70 @@ const postcssFilter = (cssCode, done) => {
     );
 };
 
+const resizeImages = async () => {
+  const data = fs.readFileSync('./src/_data/portfolioGallery.json', 'utf8');
+  const imageArray = JSON.parse(data);
+
+  const srcsetArray = [];
+
+  for (const image of imageArray) {
+    try {
+      const url = `./images/portfolio-gallery/${image}`;
+      const stats =  await Image(url, {
+        widths: [300, 430, 600, 770, 940, "auto"],
+        formats: ["webp"]
+      });
+
+      const srcset = stats.webp.map((stat, index, array) => {
+        return stat.srcset;
+      });
+
+      srcsetArray.push(srcset);
+    } catch(err) {
+      console.error(err);
+    }
+  }
+  
+  fs.writeFileSync('./src/_data/resizedPortfolioGallery.json', JSON.stringify(srcsetArray), (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("File written successfully");
+    }
+  });
+  let url = './images/portfolio-gallery/barndoors-1.jpeg';
+  let stats = await Image(url, {
+    widths: [300, 430, 600, 770, 940, "auto"],
+    formats: ["webp"]
+  });
+
+  // console.log(stats);
+};
+
+// resizeImages();
+
 module.exports = function (config) {
   config.addWatchTarget("./src/_includes/styles/tailwind.css");
   config.addNunjucksAsyncFilter("postcss", postcssFilter);
   config.addPassthroughCopy("./src/bundle.js");
   config.addPassthroughCopy("./images");
+  config.addPassthroughCopy("./img");
   config.addPlugin(eleventyNavigationPlugin);
+  // config.addShortcode("image", async function(src, alt, sizes) {
+  //   let metadata = await Image(src, {
+  //     widths: [430, 600, 770, 940],
+  //     formats: ["webp", "jpeg"]
+  //   });
+
+  //   let imageAttributes = {
+  //     alt,
+  //     sizes,
+  //     loading: "lazy",
+  //     decoding: "async"
+  //   };
+
+  //   return Image.generateHTML(metadata, imageAttributes);
+  // });
   return {
     passthroughFileCopy: true,
     dir: {
